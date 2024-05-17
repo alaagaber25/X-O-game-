b2548d88f3329be13e5631fd4ff4f00582df4bd5
aliabdelmonam <85188653+aliabdelmonam@users.noreply.github.com>
2024-05-18 02:35:56 +0300

    Add files via upload

--- X-O/XO Game.c
+++ X-O/XO Game.c
@@ -3,18 +3,31 @@
 #include "GPIO.h"
 #include "Screen.h"
 #include "game.h"
+#include "TExaS.h"
 
 extern int SW1, SW2;
-
+void GPIOPortB_Handler(void)
+{
+  
+    if (GPIO_PORTB_MIS_R & 0x10) 
+    {                            
+			GPIO_PORTB_ICR_R = 0x10;
+      SW1 = 1;
+    }
+		else if (GPIO_PORTB_MIS_R & 0x01) 
+		{                                 
+			GPIO_PORTB_ICR_R = 0x01;
+			SW2 = 1;
+    }
+}
 int main(void)
 {
 	
-	SW1_Init();
-	SW2_Init();
 	Nokia5110_Init();
 	Nokia5110_DisplayBuffer();      
 	showIntro();
 	reset();
+	GPIO_Init(&PortBconfg,GPIO_PORT_B);
 	while(1){
 		SW1 = 0, SW2 = 0;
 		WaitForInterrupt();
